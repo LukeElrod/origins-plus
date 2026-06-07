6fca0c36d6e3715f21680919d52cc56fb19f7e13
Luke Elrod <luke.elrod.career@gmail.com>
2026-06-07 13:33:21 -0700

    switch to data mod

---
+++ src/main/java/com/lukeelrod/originsplus/Origins.java
@@ -0,0 +1,11 @@
+package com.lukeelrod.originsplus;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod("originsplus")
+public class Origins {
+    public Origins(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
